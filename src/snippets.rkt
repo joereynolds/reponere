f7b9d2f3e079f6fa7d6e1f53397b868c8ebc746b
Joe Reynolds <joereynolds952@gmail.com>
2018-03-21 00:10:17 +0000

    remove dead function, extract a method

--- src/snippets.rkt
+++ src/snippets.rkt
@@ -9,19 +9,21 @@
 (define (get-snippets snippet-directory)
   (map path->string (directory-list snippet-directory)))
 
-;The holy land
 (: trigger-snippet (-> Any))
 (define (trigger-snippet snippet-directory clean-log-path)
   (map (lambda (snippet)
          (when (file-exists? clean-log-path)
-           (when (string-contains? (file->string clean-log-path)
-                                   (string-append snippet "<l-ctrl><space>"))
+           (when (log-contains-snippet? clean-log-path snippet)
              (trigger-snippet-for-word snippet-directory snippet)
              (delete-file clean-log-path)
              (process "echo '' > raw-log.log"))))
          (get-snippets snippet-directory)))
 
+(define (log-contains-snippet? log-path snippet)
+  (string-contains? (file->string log-path)
+                    (string-append snippet "<l-ctrl><space>")))
+
 (define (trigger-snippet-for-word snippet-directory word)
-   (process (build-xdotool-command
-              (file->string
-                (string-append snippet-directory word)))))
+  (process (build-xdotool-command
+             (file->string
+               (string-append snippet-directory word)))))
--- src/xtools.rkt
+++ src/xtools.rkt
@@ -1,10 +1,10 @@
 #lang typed/racket/no-check
 
-(provide get-xdotool-path build-xdotool-command start-xinput xtools-running? kill-xinput)
+(provide get-xdotool-path build-xdotool-command start-xinput kill-xinput)
 
 (: start-xinput (-> String Any))
 (define (start-xinput log-path)
-  (let ([command (string-append "xinput test 14 >> " log-path)])
+  (let ([command (string-append "xinput test 10 >> " log-path)])
     (process command)))
 
 (: get-xdotool-path (-> String))
@@ -17,11 +17,6 @@
     ; TODO escape word so characters don't break. This
     ; will probably mean reworking this solution.
     (get-xdotool-path) " type " word))
-
-(define (xtools-running?)
-  (let ([output (read-line (first (process "ps -aux | grep 'xinput' | wc -l")))])
-    ; Not entirely sure why this is 2, thought it should've been 1? (shrug)
-    (> (string->number output) 2)))
 
 (define (kill-xinput)
   (process "killall xinput"))
